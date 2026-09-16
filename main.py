ba8f5cccb2d3a97ed8f0f2c193ebe0efe901b434
cubikacajas <cubikacajas@gmail.com>
2026-09-16 20:15:56 -0300

    Agregar búsqueda en Google a oportunidades
    Updated opportunities HTML to include Google search links.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import json
 import html
-from urllib.parse import urlencode
+from urllib.parse import urlencode, quote_plus
 from urllib.request import Request as URLRequest, urlopen
 
 from fastapi import FastAPI, Request
@@ -810,9 +810,11 @@ async def dashboard():
 
         if search_opportunities:
             opportunities_items = "".join(
-                f"<li>{html.escape(opportunity)}</li>"
-                for opportunity in search_opportunities
-            )
+    f'<li>{html.escape(opportunity)} '
+    f'<a href="https://www.google.com/search?q={quote_plus(opportunity)}" '
+    f'target="_blank">Buscar en Google</a></li>'
+    for opportunity in search_opportunities
+)
 
             opportunities_html = f"""
             <div class="search-opportunities">
