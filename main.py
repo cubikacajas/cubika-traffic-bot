1b987f0be6562af9cce0806e80023f3d2042cf04
cubikacajas <cubikacajas@gmail.com>
2026-09-21 15:32:29 -0300

    Corregir indentacion y restaurar Cajas Bombones

--- main.py
+++ main.py
@@ -262,21 +262,21 @@ async def category_apply_test(category_id: int):
             category_name
         )
 
-                category_data = {
-            "name": {
-                "es": "Cajas Bombones"
-            },
-            "description": {
-                "es": "Cajas Bombones de 1/4 Kg y 1/2 Kg con y sin visor blancas e impresas "
-            },
-            "parent": 40290245,
-            "seo_title": {
-                "es": "Cajas para Bombones | CUBIKACAJAS"
-            },
-            "seo_description": {
-                "es": "Cajas para bombones y chocolates en cartulina, disponibles en distintos modelos para regalos y emprendimientos. Conocelas en CUBIKACAJAS."
-            },
-        }
+            category_data = {
+        "name": {
+            "es": "Cajas Bombones"
+        },
+        "description": {
+            "es": "Cajas Bombones de 1/4 Kg y 1/2 Kg con y sin visor blancas e impresas "
+        },
+        "parent": 40290245,
+        "seo_title": {
+            "es": "Cajas para Bombones | CUBIKACAJAS"
+        },
+        "seo_description": {
+            "es": "Cajas para bombones y chocolates en cartulina, disponibles en distintos modelos para regalos y emprendimientos. Conocelas en CUBIKACAJAS."
+        },
+    }
 
         result = update_category(
             category_id,
