cc905903412801bd807d8d06c119d4b53728f7df
cubikacajas <cubikacajas@gmail.com>
2026-09-21 16:11:16 -0300

    Usar PATCH seguro para actualizar categorias

--- main.py
+++ main.py
@@ -130,7 +130,7 @@ def update_category(category_id, category_data):
             "User-Agent": "CUBIKA TRAFFIC BOT",
             "Content-Type": "application/json",
         },
-        method="PUT",
+        method="PATCH",
     )
 
     with urlopen(request, timeout=20) as response:
